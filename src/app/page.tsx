7c9afef741ab8ba0aaf1c3304ada36189c0fb562
Yiğit Gülmez <ygulmez873@gmail.com>
2025-04-03 00:18:22 +0300

    any changes

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const wrapper = "bg-blue-500 text-white px-4 py-2 rounded";
+export default function Home() {
+  return (
+    <main>
+      <div >
+
+      </div>
+    </main>
+  );
+}
