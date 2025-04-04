55fb0bc2cc171790a473b2f2a420b066a8db6e1f
Yiğit Gülmez <ygulmez873@gmail.com>
2025-04-04 22:36:04 +0300

    bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,22 +1,21 @@
 'use client'
 
 import { Button, ButtonGroup } from "@heroui/button"
-import { usePathname, useRouter } from "next/navigation"
-
-
+import { useRouter } from "next/navigation"
 
 export default function Navbar() {
   const router = useRouter();
-  const pathname = usePathname();
-  const changePage = () => {
-    router.push(pathname == '/rollcall' ? '/camera' : '/rollcall')
+  const changePage = (route: string) => {
+    if (route !== window.location.pathname) {
+      router.push(route);
+    }
   };
   return (
     <div className="fixed flex items-center w-full h-16">
       <div className="flex justify-center items-center w-full">
         <ButtonGroup color="primary" variant="shadow" radius="sm">
-          <Button onPress={changePage}>Roll Call</Button>
-          <Button onPress={changePage}>Camera</Button>
+          <Button onPress={() => changePage('/rollcall')}>Roll Call</Button>
+          <Button onPress={() => changePage('/camera')}>Camera</Button>
         </ButtonGroup>
       </div>
     </div>
