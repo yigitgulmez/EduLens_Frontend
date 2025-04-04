8d4d43596bda9fa424ddc90f8e0c1b2b236f5f6e
Yiğit Gülmez <ygulmez873@gmail.com>
2025-04-04 22:36:38 +0300

    update

--- app/rollcall/page.tsx
+++ app/rollcall/page.tsx
@@ -4,7 +4,6 @@ import { Button } from '@heroui/button';
 import { DatePicker } from "@heroui/date-picker";
 import { Select, SelectItem } from '@heroui/select';
 
-
 export const branchs = [
   {key: "cat", label: "Cat"},
   {key: "dog", label: "Dog"},
@@ -44,25 +43,32 @@ function page() {
         <div className="flex justify-between px-5">
           <div className='flex gap-5'>
             <div>
-              <Select className="w-24" label='Class' items={studentClasses}>
+              <Select className="w-24" placeholder='Class' items={studentClasses}>
                 {(studentClass) => <SelectItem>{studentClass.label}</SelectItem>}
               </Select>
             </div>
             <div>
-              <Select className="w-24" label='Branch' items={branchs}>
+              <Select className="w-24" placeholder='Branch' items={branchs}>
                 {(branch) => <SelectItem>{branch.label}</SelectItem>}
               </Select>
             </div>
           </div>
           <div>
-          <DatePicker className="w-32" label="Date"/>
-          
+            <DatePicker className="w-32 h-"/>
           </div>
         </div>
-        <div className="w-full h-full border-2 border-bgSecondary rounded-2xl flex flex-col overflow-y-scroll p-2">
+        <div className="w-full h-full border-2 border-bgSecondary rounded-2xl flex flex-col gap-2 overflow-y-auto p-2 pr-3">
         {/* {students.map((item) => (
-            <Student img={item.img} name={item.name} number={item.number}/>
+            <Student avatar={item.avatar} name={item.name} number={item.number}/>
           ))} */}
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
+          <Student avatar={"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"} name={"Name"} number={101}/>
         </div>
         <div className='px-5 '>
           <Button className='px-3 py-1 bg-white' radius='sm'>Save</Button>
--- components/Student.tsx
+++ components/Student.tsx
@@ -1,23 +1,24 @@
-import Image from "next/image"
+import {Checkbox} from "@heroui/checkbox";
+import {Avatar, AvatarGroup, AvatarIcon} from "@heroui/avatar";
+import { useState } from "react";
 
 interface Props {
-  img: string,
+  avatar: string,
   name: string,
   number: number
 }
-const Student: React.FC<Props> = ({img, name, number}) => {
+const Student: React.FC<Props> = ({avatar, name, number}) => {
   return (
-    <div className="flex justify-between">
-      <div>
-        <Image src={img} alt={name} width={50} height={50}/>
-        <div className="flex ms-3">
-          <div className="text-textPrimary">{name}</div>
-          <div className="text-textTertiary">{number}</div>
+    <div className="flex items-center p-2 bg-bgTertiary rounded-xl select-none">
+      <Checkbox size="lg" radius="sm" className="flex flex-row-reverse justify-between min-w-full min-h-full ">
+        <div className="flex">
+          <Avatar size="lg" showFallback  src={avatar}/>
+          <div className="flex flex-col ms-3">
+            <div className="text-textPrimary">{name}</div>
+            <div className="text-textTertiary">{number}</div>
+          </div>
         </div>
-      </div>
-      <div>
-        ...checkbox
-      </div>
+      </Checkbox>
     </div>
   )
 }
