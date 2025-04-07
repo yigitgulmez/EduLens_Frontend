c3c08606cea8ab51bf93fdb92e146790a4e25684
Yiğit Gülmez <ygulmez873@gmail.com>
2025-04-07 10:16:26 +0300

    connection requirements met

--- app/rollcall/page.tsx
+++ app/rollcall/page.tsx
@@ -3,6 +3,7 @@ import Student from '@/components/Student';
 import { Button } from '@heroui/button';
 import { DatePicker } from "@heroui/date-picker";
 import { Select, SelectItem } from '@heroui/select';
+import { useEffect, useState } from 'react';
 
 export const branchs = [
   {key: "cat", label: "Cat"},
@@ -37,24 +38,45 @@ export const studentClasses = [
 ];
 
 function page() {
+  const [studentClass, setStudentClass] = useState<String | undefined>()
+  const [branch, setBranch] = useState<String | undefined>()
+  const [date, setDate] = useState<number | undefined>()
+  useEffect(() => {
+    if (studentClass || branch || date) {
+      // updateTable(studentClass, branch, date)
+    }else {
+      
+    }
+  
+  }, [studentClass, branch, date])
+  
   return (
     <main className='h-screen w-screen flex justify-center items-center'>
       <div className='bg-primary-400 text-textSecondary w-[600px] h-2/4 rounded-3xl px-7 py-5 flex flex-col gap-4'>
         <div className="flex justify-between px-5">
           <div className='flex gap-5 *:transition-all *:duration-200 *:transform active:*:scale-[98%]'>
             <div>
-              <Select className="w-24" placeholder='Class' items={studentClasses}>
+              <Select className="w-24" onChange={(e) => setStudentClass(e.target.value)} placeholder='Class' items={studentClasses}>
                 {(studentClass) => <SelectItem>{studentClass.label}</SelectItem>}
               </Select>
             </div>
             <div>
-              <Select className="w-24" placeholder='Branch' items={branchs}>
+              <Select className="w-24" onChange={(e) => setBranch(e.target.value)} placeholder='Branch' items={branchs}>
                 {(branch) => <SelectItem>{branch.label}</SelectItem>}
               </Select>
             </div>
           </div>
           <div>
-            <DatePicker className="w-32 transition duration-300 transform active:scale-[98%]"/>
+            <DatePicker 
+                onChange={(value) => {
+                if (value && "toDate" in value) {
+                  const timestamp = value.toDate("UTC").getTime();
+                  setDate(timestamp);
+                } else {
+                  setDate(undefined);
+                }
+              }}
+            className="w-32 transition duration-300 transform active:scale-[98%]"/>
           </div>
         </div>
         <div className="w-full h-full border-2 border-bgSecondary rounded-s-2xl flex flex-col gap-2 overflow-y-auto p-2 pr-3">
---
+++ app/utils/updateTable.tsx
@@ -0,0 +1,3 @@
+export default function name(studentClass:string, branch:string, date:number) {
+  
+}
