98314d0350e56999b0b901d287fd452cdd22f744
Yiğit Gülmez <ygulmez873@gmail.com>
2025-04-28 09:34:14 +0300

    Update students.ts

--- utils/students.ts
+++ utils/students.ts
@@ -139,7 +139,6 @@ export async function getAttendanceID(id: string | undefined, date: number | und
   try {
     const data = await fetchData(id, date);
     console.log("attendance get data", data);
-    console.log("attandanteData: ", data);
     return data?.id;
   } catch (error) {
     console.error("Error fetching attendance data:", error);
